a8b42e602100bd74b0190c20f18ade477c13e56a
Dominic01prog <djs7129@psu.edu>
2020-09-18 02:53:26 +0000

    added {sum} is

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ def digit_sum(n):
 def run():
   sum = int(input("Enter an int: "))
   digit_sum(sum)
-  print(f"sum of digits is {digit_sum(sum)}.")
+  print(f"sum of digits of {sum} is {digit_sum(sum)}.")
 if __name__ == "__main__":
   run()
